760aa5fcd28ccc60bdb0d6ecb4221762cbf02d11
liuzhangren100 <759551480@qq.com>
2019-11-16 16:56:23 +0800

    update

--- src/pages/About/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import ReactDom from 'react-dom';
-
-import style from './style';
-
-export default () => {
-  return <div {...style.aboutTitle}>this is AboutPage</div>;
-};
--- src/pages/Home/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import ReactDom from 'react-dom';
-
-export default () => {
-  return <div>this is HomePage</div>;
-};
